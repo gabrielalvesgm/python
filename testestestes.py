afa3480305ef513a51b557473e6179a51679cbd1
GabrielAlvesGM <lgabrielalvesgm@gmail.com>
2024-12-26 12:16:59 -0300

    att

--- testestestes.py
+++ testestestes.py
@@ -1,15 +1,9 @@
-texto = "Olá, Python!"
-tamanho = len(texto)
-print("O comprimento da string é:", tamanho)
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+impares = [1, 3, 5, 7, 9]
+pares = [2, 4, 6, 8, 10]
 
-numeros = [1, 2, 3, 4, 5]
-quantidade = len(numeros)
-print("O número de elementos na lista é:", quantidade)
-
-coordenadas = (10, 20, 30)
-elementos = len(coordenadas)
-print("O número de elementos na tupla é:", elementos)
-
-dados = {'nome': 'Maria', 'idade': 25, 'cidade': 'Exemplo'}
-chaves = len(dados)
-print("O número de chaves no dicionário é:", chaves)
+for x in numeros:
+    if x in impares:
+        print(f"Este número {x} é ímpar")
+    else:
+        print(f"Este número {x} é par")
